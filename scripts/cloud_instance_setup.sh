9d802ae20b3ff58e68b4ba6e339640d6dbac7a87
epickrram <mark@epickrram.com>
2018-02-08 19:53:22 +0000

    Fix overrides

--- scripts/cloud_instance_setup.sh
+++ scripts/cloud_instance_setup.sh
@@ -3,8 +3,6 @@ set -euxo pipefail
 
 sudo apt install cpuset openjdk-8-jdk-headless git hwloc
 
-
-
 cd
 
 lstopo-no-graphics -of xml -v topo.xml
@@ -24,10 +22,14 @@ unzip ../dist.zip
 export OVERRIDE_IRQ_CPUS=1,4,5
 export OVERRIDE_RESTRICTED_CPUS=2,3,6,7
 export OVERRIDE_SYSTEM_CPUS=0,1,4,5
-CLIENT_IN_CPU=2
-CLIENT_OUT_CPU=3
-SERVER_IN_CPU=6
-SERVER_OUT_CPU=7
-
+export OVERRIDE_CLIENT_IN_CPU=2
+export OVERRIDE_CLIENT_OUT_CPU=3
+export OVERRIDE_SERVER_IN_CPU=6
+export OVERRIDE_SERVER_OUT_CPU=7
 
 bash ./mask_cpus.sh
+
+#bash ./cpuset_wrapper.sh ./run_tcp_server.sh
+#bash ./cpuset_wrapper.sh ./run_tcp_client.sh
+#bash ./cpuset_wrapper.sh ./run_udp_server.sh
+#bash ./cpuset_wrapper.sh ./run_udp_client.sh
